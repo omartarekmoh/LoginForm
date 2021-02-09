bff5bf1e95b1aecff8ba73e3e16f4637eb348993
omartarekmoh <omartarekmoh1@gmail.com>
2021-02-09 22:21:27 +0200

    Login Form and checking data base edited

--- VALIDATE/sign_in.php
+++ VALIDATE/sign_in.php
@@ -1,28 +1,4 @@
 <?php
-// $url = 'http://localhost/LoginForm/Main/signin.php';
-
-// // what post fields?
-// $fields = array(
-//   'username' => "none",
-// );
-
-// // build the urlencoded data
-// $postvars = http_build_query($fields);
-
-// // open connection
-// $ch = curl_init();
-
-// // set the url, number of POST vars, POST data
-// curl_setopt($ch, CURLOPT_URL, $url);
-// curl_setopt($ch, CURLOPT_POST, count($fields));
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $postvars);
-
-// // execute post
-// $result = curl_exec($ch);
-
-// // close connection
-// curl_close($ch);
-
 require_once("../DB/db.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $arr = get_data("SELECT password FROM `user_data` WHERE Email = \"" . $_POST["user"] . "\" OR userName = \"" . $_POST["user"] . "\"");
